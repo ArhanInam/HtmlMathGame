89d15b0a0b55bc1c32ab16e06ad93c156dba145a
Arhan Inam <91776437+ArhanInam@users.noreply.github.com>
2022-06-30 05:39:48 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+Player1 ="";
+Player2 ="";
+
+function adduser() {
+    localStorage.setItem("player_1_input_name" Player1);
+    localStorage.setItem("player_2_input_name" Player2);
+    window.location="math.html";
+}
